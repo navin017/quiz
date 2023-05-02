01c64980a52038df6840c7c66671b80e88d240a9
navin017 <127098515+navin017@users.noreply.github.com>
2023-05-02 18:57:53 +0530

    Add files via upload

---
+++ quizzz/quiz/quiz/quiz/quiz/src/StarterPage.js
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+// function StarterPage() {
+//   return (
+//     <form className='cover'>
+    
+              
+//     </form>
+//   )
+// }
+
+// export default StarterPage
