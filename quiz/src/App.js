2085cd9bfb8deac9ab0b1aca9ea41b09575d0c78
navin017 <127098515+navin017@users.noreply.github.com>
2023-05-05 18:09:12 +0530

    Add files via upload

---
+++ quiz/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React from 'react'
+import Quiz from './QuizApp';
+import StartPage from './StartPage';
+import { createBrowserRouter ,RouterProvider} from 'react-router-dom';
+
+const route = createBrowserRouter([
+  {path:'/',element:<StartPage/>},
+  {path:'/quiz',element:<Quiz/>}
+])
+function App() {
+  
+  
+  return (
+    <>
+   <RouterProvider router = {route}/>
+   
+    </>
+   )
+          
+ 
+}
+
+export default App;
